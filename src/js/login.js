ace6755d1dc6068330679022e9fe30ea73fc49da
Gabriela Bastos <gabrielamobastos@gmail.com>
2025-09-16 18:36:02 -0300

    feat(login): ajustes de responsividade no css e pequenas correçoes de nomes no JS

--- src/js/login.js
+++ src/js/login.js
@@ -7,9 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
         const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value.trim();
+        const senha = document.getElementById("senha").value.trim();
         
-        if(!email || !password){
+        if(!email || !senha){
             alert("Preencha todos os campos!")
             return;
         }
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const response = await fetch("http://localhost:8080/api/login" , {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({email, password})
+                body: JSON.stringify({email, senha})
             });
 
             if(response.ok){
